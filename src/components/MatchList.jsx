c9317f03f0af166860b9c3e06b1408325fda9b1e
Guigan713 <guillaume.lequin713@gmail.com>
2021-08-05 19:32:24 +0200

    add matchlist

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,10 +1,33 @@
+import axios from 'axios'
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import MatchCard from './components/MatchCard'
 
 
 const MatchList = () => {
-    return (
-        <div>
+    const [listMatch, setListMatch] =useState([]);
+    const { id } = useParams();
 
+    useEffect(() => {
+        const getMatchList = () => {
+
+            axios.get(`https://miadil.github.io/starwars-api/api/id/${id}.json`)
+            .then(response => response.data)
+            .then(data => {
+                console.log(data)
+                setListMatch(data);
+            })
+        }
+        getMatchList()
+    }, [id])
+
+
+
+    return (
+        <div className="matchListContainer">
+            <MatchCard name={listMatch}/>
         </div>
     );
 }
